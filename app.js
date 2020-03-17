1c8bbb2b7ac885ce1e21d4a3db790e28620687da
chronos25 <ritikrajsrivastava90@gmail.com>
2020-03-17 06:41:55 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+var gplay = require('google-play-scraper');
+ 
+gplay.list({
+    category: gplay.category.APPLICATION,
+    collection: gplay.collection.TOP_FREE,
+    num: 2
+  })
+  .then(console.log, console.log);
+
+  module.exports = app;
