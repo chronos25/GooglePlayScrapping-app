f7339d7de5728507fd2cfa9ce0697594c68f3e03
chronos25 <ritikrajsrivastava90@gmail.com>
2020-03-22 13:27:42 +0530

    completed applists services and defined put and post routes with working

--- routes/listRouter.js
+++ routes/listRouter.js
@@ -4,10 +4,12 @@ const express = require('express'),
  gplay = require('google-play-scraper'),
  list = require('../controller/getLists'),
  appList = require('../models/list');
-
 scraper.route('/')
 .get((req, res, next) =>{ 
      appList.find({}).then((applists)=>{
+         console.log(
+            Object.keys(applists).length);
+         
          res.status = 200;
          res.setHeader('Content-Type','application/json');
          res.json(applists);
@@ -37,35 +39,63 @@ scraper.route('/')
            
            appsList.save(data[i]).then((app)=>{
                console.log("RECEIVED DATA");
-                res.status = 200;
-               
            },(err)=>next(err))
            .catch((err)=>{
                console.log(err);
                next(err);
            });
        }
+      res.status = 200;
+      res.setHeader('Content-Type','application/json');
+      res.json(result);
     },(err)=>next(err)).catch((err)=>{
         console.log("Error");
         next(err);
     })
-      res.setHeader('Content-Type','application/json');
-      res.json(result);
 })
 
-// .put((req,res,next)=>{
-//     list.find({}).then((applist)=>{
-//         gplay.list({
-//             category: gplay.category.APPLICATION, 
-//             collection: gplay.collection.TOP_FREE,
-//             num: 15
-//         })
-//         .then((data)=>{
-//             for(i=0;i<15;i++){
-//             }
-//       }).catch((err)=>{console.log(err);});
-//     })
-// })
+.put((req,res,next)=>{   
+    appList.find({}).then((applist)=>{
+        
+        lastIndex = Object.keys(applist).length;
+        gplay.list({
+            category: gplay.category.APPLICATION, 
+            collection: gplay.collection.TOP_FREE,
+            num: lastIndex+1
+        })
+        .then((data)=>{ 
+            // for(i=0;i<lastIndex;i++)
+            // {
+                // console.log(lastIndex);
+                 console.log(applist[lastIndex-1].title);
+                 console.log(data[lastIndex].title);
+                
+                if(applist[lastIndex-1].title!=data[lastIndex].title)
+                {
+                    let appsList = new appList({
+                        title: data[lastIndex].title,
+                        appId: data[lastIndex].appId,
+                        icon: data[lastIndex].icon,
+                        score: data[lastIndex].score,
+                        developer: data[lastIndex].developer
+                    });
+                    
+                    appsList.save(data[lastIndex]).then((app)=>{
+                        console.log("RECEIVED DATA");
+                    },(err)=>next(err))
+                    .catch((err)=>{
+                        console.log(err);
+                        next(err);
+                    });
+                }
+           // }
+            res.status(200);
+            res.setHeader('Content-Type','application/json');
+            res.json({success:true});
+
+      }).catch((err)=>{console.log(err);});
+    })
+})
 
 .delete((req,res,next)=>{
     appList.remove({}).then((result) =>{
